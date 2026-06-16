9f9ff90fc431589efe2ad8d0f54196203d3ccbab
MelissACJ <melissacampos1214@gmail.com>
2026-06-15 21:54:53 -0500

    soporte para neon

--- config/database.js
+++ config/database.js
@@ -1,15 +1,26 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: 'postgres',
-    logging: false
-  }
-);
+const sequelize = process.env.DATABASE_URL
+  ? new Sequelize(process.env.DATABASE_URL, {
+      dialect: 'postgres',
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false
+        }
+      },
+      logging: false
+    })
+  : new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        host: process.env.DB_HOST,
+        dialect: 'postgres',
+        logging: false
+      }
+    );
 
-module.exports = sequelize; // Exporta la instancia de Sequelize para usarla en otros archivos
+module.exports = sequelize;
